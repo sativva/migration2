fa18f94c3aabd22fa1ac0a02e14ad894dab8efdd
thomas rondio <sativva@free.fr>
2020-01-15 02:41:14 +0100

    retry

--- app/controllers/lpb_controller.rb
+++ app/controllers/lpb_controller.rb
@@ -482,7 +482,7 @@
         localfile = File.basename(file)
         ftp.getbinaryfile(file, localfile, @blocksize)
         csv = CSV.open(localfile, headers: false,liberal_parsing: true)
-        csv.each_with_index do |line, i|
+        csv.drop(1700).each_with_index do |line, i|
           sleep(0.5)
           next if i == 0
           lili = line.join(',').to_s.gsub(/\"/, "").split(';')
